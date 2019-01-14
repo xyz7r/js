7a2a2d3204b648396f36f3123103534787c8a150
xyz7r <hafidza97jr@gmail.com>
2019-01-14 08:00:37 +0700

    learn javascript

---
+++ learn-js/beginners/math-operator/index.js
@@ -0,0 +1,77 @@
+/*
+
+assignment operator
+
+= equal
++ addition
+- subtraction
+/ division
+* multiplication
+
+*/
+
+// equal
+var myVar = 9;
+console.log("My Variable", myVar);
+
+// addtion
+var addition = myVar + 10;
+console.log("addition", addition);
+
+// subtraction
+var subtraction = myVar - 10;
+console.log("subtraction", subtraction);
+
+// division
+var division = myVar / 10;
+console.log("division", division);
+
+// multiplication
+var multiplication = myVar * 10;
+console.log("multiplication", multiplication);
+
+console.log("===========================================================");
+
+// Math Operator Short-Hand
+
+// equal
+var myVar = 9;
+console.log("My Variable", myVar);
+
+// addtion
+myVar += 10;
+console.log("addition Short-Hand", addition);
+
+// subtraction
+myVar -= 10;
+console.log("subtraction Short-Hand", subtraction);
+
+// division
+myVar /= 10;
+console.log("division Short-Hand", division);
+
+// multiplication
+myVar *= 10;
+console.log("multiplication Short-Hand", multiplication);
+
+
+console.log("===========================================================");
+console.log("My Variable", myVar);
+// Increment Operator
+// Postfix
+myVar++
+console.log("Postfix Increment ", myVar);
+
+//Prefix
+++myVar
+console.log("Prefix Increment ", myVar);
+
+// Decrement Operator
+// Postfix
+myVar--
+console.log("Postfix Decrement ", myVar);
+
+//Prefix
+--myVar
+console.log("Prefix Decrement ", myVar);
+
---
+++ learn-js/beginners/variabel/index.js
@@ -0,0 +1,9 @@
+var myVar1;
+myVar1 = 10;
+
+var myVar2 = "hello";
+console.log("myVar2 : ", myVar2);
+
+// Change Value
+myVar2 = "myvar2 has chaneced";
+console.log("myVar2 : ", myVar2);
