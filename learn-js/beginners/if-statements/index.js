92d90b2bfb0829a7b93ebbfb4a48eda9af4fb679
xyz7r <hafidza97jr@gmail.com>
2019-01-15 13:16:49 +0700

    add folder if-statement

---
+++ learn-js/beginners/if-statements/index.js
@@ -0,0 +1,60 @@
+/*
+
+If Statement
+
+
+if (condition) {
+    //Statement 
+}
+
+*/
+var a = 10;
+
+if (a == 10) {
+    //Statement
+    console.log("Correct !!!");
+}
+
+/*---------------------------------------------------------*/
+/*
+If Else Statement
+
+if (condition) {
+    //Statment 1
+} else {
+    //Statment 2
+}
+
+*/
+var b = 9
+
+if (b == 10) {
+    console.log("Correct!!!");
+} else {
+    console.log("Please Check Your Number!!!");
+}
+
+
+/*---------------------------------------------------------*/
+/*
+If ... Else If ... Else Statement
+
+if (condition) {
+    //Statment 1
+} else if(condition){
+    //Statment 2
+}else{
+    //Statment 3
+}
+
+*/
+
+var c = 9
+
+if (c == 10) {
+    console.log("Correct!!!");
+} else if (c == 9) {
+    console.log("Correct!!!");
+} else {
+    console.log("Please Check Your Number!!!");
+}
